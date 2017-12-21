37403257a70d138198c046d50f92247ac9fa3c24
S.T <tanaka-shu@mc24441-bp15.gndomain.gnavi.co.jp>
2017-12-21 09:29:20 +0900

    repair build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,22 @@
+# set
+OS=${1}
+
 # resource file copy
 cp -R ../src/resources ../bin/macshare/
 cp ../src/config/user.json ../bin/macshare/config/
 
+# remove binary file
+rm ../bin/macshare/macshare*
+
 # compile
 cd ../src/main/
-GOOS=windows GOARCH=amd64 go build -o ../../bin/macshare/macshare.exe main.go
+case ${OS} in
+    "linux" ) GOOS=linux GOARCH=amd64 go build -o ../../bin/macshare/macshare main.go
+    ;;
+    "windows" ) GOOS=windows GOARCH=amd64 go build -o ../../bin/macshare/macshare.exe main.go
+    ;;
+    "mac" ) GOOS=darwin GOARCH=amd64 go build -o ../../bin/macshare/macshare main.go
+    ;;
+    * ) echo "No compile"
+    ;;
+esac
